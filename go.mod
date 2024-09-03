937865ed3595013dbbfb5ad4b06d6d1212a2b296
Ell534 <elliot94b@gmail.com>
2024-09-03 14:06:33 +0100

    add function to normalize URLs with passing tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Ell534/goWebcrawler
+
+go 1.22.5
---
+++ normalize_url.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	//"errors"
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+func normalizeURL(fullURL string) (string, error) {
+	parsedURL, err := url.Parse(fullURL)
+	if err != nil {
+		return "", fmt.Errorf("error parsing given URL: %w", err)
+	}
+	normalizedURL := parsedURL.Hostname() + parsedURL.Path
+	normalizedURL = strings.ToLower(normalizedURL)
+	normalizedURL = strings.TrimSuffix(normalizedURL, "/")
+	return normalizedURL, nil
+}
