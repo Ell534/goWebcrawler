aa5d42eba2c0dd9f568d6bd52e33d3958f09fec6
Ell534 <elliot94b@gmail.com>
2024-09-03 18:29:29 +0100

    add logic for checking command line arguments

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	rawBaseURL := os.Args[1]
+
+	fmt.Printf("starting crawl of: %s", rawBaseURL)
+
+	// if len(os.Args) == 2 {
+	// 	fmt.Printf("starting crawl of: %s", os.Args[1])
+	// }
 }
