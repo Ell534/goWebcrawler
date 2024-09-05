91c5832d88144536b4293cfdc320bd099d991e53
Ell534 <elliot94b@gmail.com>
2024-09-05 12:47:21 +0100

    refactor getURLs and test into their own files

--- normalize_url.go
+++ normalize_url.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	//"errors"
 	"fmt"
-	"golang.org/x/net/html"
 	"net/url"
 	"strings"
 )
@@ -19,40 +17,40 @@ func normalizeURL(fullURL string) (string, error) {
 	return normalizedURL, nil
 }
 
-func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
-	baseURL, err := url.Parse(rawBaseURL)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing rawBaseURL: %w", err)
-	}
-
-	htmlReader := strings.NewReader(htmlBody)
-	doc, err := html.Parse(htmlReader)
-	if err != nil {
-		return []string{}, fmt.Errorf("error parsing htmlBody into parse tree: %w", err)
-	}
-
-	var urls []string
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key == "href" {
-					href, err := url.Parse(a.Val)
-					if err != nil {
-						fmt.Printf("could not parse href '%v': %v\n", a.Val, err)
-						continue
-					}
-
-					resolvedURL := baseURL.ResolveReference(href)
-					urls = append(urls, resolvedURL.String())
-				}
-			}
-		}
-
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
-		}
-	}
-	f(doc)
-	return urls, nil
-}
+// func getURLsFromHTML(htmlBody, rawBaseURL string) ([]string, error) {
+// 	baseURL, err := url.Parse(rawBaseURL)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error parsing rawBaseURL: %w", err)
+// 	}
+//
+// 	htmlReader := strings.NewReader(htmlBody)
+// 	doc, err := html.Parse(htmlReader)
+// 	if err != nil {
+// 		return []string{}, fmt.Errorf("error parsing htmlBody into parse tree: %w", err)
+// 	}
+//
+// 	var urls []string
+// 	var f func(*html.Node)
+// 	f = func(n *html.Node) {
+// 		if n.Type == html.ElementNode && n.Data == "a" {
+// 			for _, a := range n.Attr {
+// 				if a.Key == "href" {
+// 					href, err := url.Parse(a.Val)
+// 					if err != nil {
+// 						fmt.Printf("could not parse href '%v': %v\n", a.Val, err)
+// 						continue
+// 					}
+//
+// 					resolvedURL := baseURL.ResolveReference(href)
+// 					urls = append(urls, resolvedURL.String())
+// 				}
+// 			}
+// 		}
+//
+// 		for c := n.FirstChild; c != nil; c = c.NextSibling {
+// 			f(c)
+// 		}
+// 	}
+// 	f(doc)
+// 	return urls, nil
+// }
